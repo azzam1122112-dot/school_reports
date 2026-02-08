4ddbd3831eab07618b2786bebd181b04beca873d
manso <manso@users.noreply.github.com>
2026-02-08 10:32:51 +0300

    138

--- reports/routing.py
+++ reports/routing.py
@@ -5,5 +5,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"^ws/notifications/$", consumers.NotificationCountsConsumer.as_asgi()),
+    # Be tolerant: depending on server/router versions the ASGI scope path may
+    # include a leading slash. Support both to avoid silent handshake rejection.
+    re_path(r"^/?ws/notifications/?$", consumers.NotificationCountsConsumer.as_asgi()),
 ]
